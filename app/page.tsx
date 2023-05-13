fb6b97e4eb080129adc926f1c7f55f9508db5a9f
ngenome <ngenondumia@gmail.com>
2023-05-13 20:18:54 +0300

    initial push

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,190 @@
-import Image from 'next/image'
+import Image from "next/image";
+import React from "react";
+import PropTypes from "prop-types";
+import AboutMe from "@/components/AboutMe";
+import ContactMe from "@/components/Contact";
+import { FaLaptopCode, FaMobileAlt } from "react-icons/fa";
 
-export default function Home() {
+interface SkillCardProps {
+  name: string;
+  level: number;
+  icon: string;
+}
+
+const SkillCard: React.FC<SkillCardProps> = ({ name, icon, level }) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="skill-card">
+      <div className="skill-info">
+        <div className="skill-icon">
+          <img
+            className="text-white fill-green-400"
+            src={icon}
+            alt={name}
+            width={50}
+            height={50}
+          />
+        </div>
+        <div className="skill-name">{name}</div>
+        {/* <div className="skill-level">{level}%</div> */}
+      </div>
+    </div>
+  );
+};
+
+SkillCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  level: PropTypes.number.isRequired,
+};
+
+function Hero() {
+  return (
+    <div className="bg-gray-900 h-screen w-screen text-white py-20">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-left">
+          <h1 className="text-4xl font-bold leading-tight tracking-tight mb-4">
+            Hi, I'm Kelvin Ng'eno
+          </h1>
+          <p className="text-2xl mb-8">A Full Stack Software Engineer</p>
+          <div className="flex justify-start">
+            <a
+              href="#about-me"
+              className="px-6 py-3 rounded-full text-gray-900 font-semibold bg-white hover:bg-gray-200 mr-4"
+            >
+              About Me
+            </a>
+            <a
+              href="#contact-me"
+              className="px-6 py-3 rounded-full bg-green-500 text-white font-semibold hover:bg-green-600"
+            >
+              Contact Me
+            </a>
+          </div>
         </div>
       </div>
+    </div>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+const ProjectView = ({
+  image,
+  links,
+  title,
+  description,
+  time,
+}: {
+  image: string;
+  links: string[];
+  title: string;
+  description: string;
+  time: string;
+}) => {
+  return (
+    <div className="bg-white w-72 m-4  shadow overflow-hidden rounded-lg hover:bg-gray-50 duration-100 transition">
+      <img
+        className="w-full h-64 object-cover object-center"
+        src={image}
+        alt={title}
+      />
+      <div className="px-4 py-5">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">{description}</p>
+        <div className="flex justify-between mt-4">
+          <div className="flex flex-col">
+            {links.map((link) => (
+              <a
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500 hover:text-blue-700"
+              >
+                {link}
+              </a>
+            ))}
+          </div>
+
+          <div className="text-gray-600">{time}</div>
+        </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+export default function Home() {
+  const projects = [
+    {
+      image: "https://www.wera.app/logo.png",
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      links: [
+        "https://www.wera.app/",
+        "https://play.google.com/store/apps/details?id=com.wera.app",
+      ],
+      title: "Wera app",
+      description:
+        "A Service and Job procurement and Ecommerce app. This is an app where you can sell and buy goods, as well as provide and / or procure services. You can also procure services too. Download on Playstore.",
+      time: "May 2023",
+    },
+    {
+      image: "/jobzy-2.png",
+      links: [
+        "https://www.jobzy.africa/",
+        "https://play.google.com/store/apps/details?id=africa.jobzy.client",
+        "https://play.google.com/store/apps/details?id=africa.jobzy.provider",
+      ],
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+      title: "Jobzy Apps",
+      description:
+        "Jobzy is a revolutionary platform that makes it easy for people to procure services and for service providers to offer their services. With two separate apps, one for clients and another for providers, Jobzy has become the go-to solution for anyone looking for a reliable and efficient way to get things done.",
+      time: "May 2023",
+    },
+    {
+      image:
+        "https://play-lh.googleusercontent.com/YtnzNcR_FpRrvxjbmIgUj9QX8G6JMnwm5qExaSJwR5Cscj2RZr6wknxcjzg2qBvVV1Bp=w416-h235-rw",
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      links: ["https://play.google.com/store/apps/details?id=com.fnotely.app"],
+      title: "Fnotely app",
+      description:
+        "Simple but efficient note taking app made with react native Expo. Download on Playstore.",
+      time: "May 2023",
+    },
+    {
+      image: "/mjenzi.png",
+      links: ["https://mjenzivendordashboard.netlify.app/"],
+      title: "MJENZI VENDOR DASHBOARD",
+      description:
+        "Vendor Management Dashboard. A platform where vendors can register, and manage the products they sell. View Website.",
+      time: "May 2023",
+    },
+  ];
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-gray-800">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Kodchasan:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet"
+      ></link>
+      <Hero />
+      <AboutMe />
+      <div>
+        <div className="ml-4">
+          <h2 className="text-2xl font-bold leading-7 hover:text-green-600 text-white sm:text-3xl sm:truncate">
+            My Projects
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="flex flex-wrap mt-1">
+            <p className="text-sm text-gray-400">
+              Here are some of the projects I have worked on.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-row flex-wrap ">
+          {projects.map((e, i) => {
+            return <ProjectView {...e} />;
+          })}
+        </div>
       </div>
+      <ContactMe />
     </main>
-  )
+  );
 }
---
+++ components/AboutMe.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { FaReact, FaMobileAlt, FaLaptopCode } from "react-icons/fa";
+import { SiDjango, SiNodedotjs } from "react-icons/si";
+import { IoLogoJavascript, IoLogoPython } from "react-icons/io";
+import { DiCss3, DiHtml5, DiPostgresql } from "react-icons/di";
+import { AiOutlineConsoleSql } from "react-icons/ai";
+import profilePic from "./profile-pic.jpg";
+
+const AboutMe = () => {
+  return (
+    <section className="w-screen bg-gray-900 text-white py-16 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto">
+        <div className="lg:flex lg:items-center lg:justify-between">
+          <div className="flex-1 min-w-0">
+            <div className="flex items-center">
+              <img
+                className="h-12 w-12 rounded-full"
+                src={"/kelvin.jpg"}
+                alt="Kelvin Ng'eno"
+              />
+              <div className="ml-4">
+                <h2 className="text-2xl font-bold leading-7 text-green-600 sm:text-3xl sm:truncate">
+                  Kelvin Ng'eno
+                </h2>
+                <div className="flex flex-wrap mt-1">
+                  <div className="flex items-center text-sm mr-4">
+                    <FaLaptopCode className="mr-1" />
+                    <span>Full Stack Developer</span>
+                  </div>
+                  <div className="flex items-center text-sm mr-4">
+                    <FaMobileAlt className="mr-1" />
+                    <span>Mobile Developer</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-4 flex flex-wrap">
+              <div className="flex items-center mr-6">
+                <FaReact className="mr-1" />
+                <span>React</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <FaReact className="mr-1" />
+                <span>React Native</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <IoLogoJavascript className="mr-1" />
+                <span>JavaScript</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <IoLogoPython className="mr-1" />
+                <span>Python</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <DiCss3 className="mr-1" />
+                <span>CSS</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <DiHtml5 className="mr-1" />
+                <span>HTML</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <DiPostgresql className="mr-1" />
+                <span>PostgreSQL</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <AiOutlineConsoleSql className="mr-1" />
+                <span>SQL</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <SiDjango className="mr-1" />
+                <span>Django</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <SiNodedotjs className="mr-1" />
+                <span>Node.js</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="h-screen items-center justify-center w-full md:p-40 ">
+        <p className="text-xl ">
+          Hi, I'm Kelvin Ng'eno, a skilled full stack software engineer with a
+          passion for developing{" "}
+          <span className="bg-green-600">innovative solutions</span>. I have a
+          deep appreciation for the nuances of software development, and I pride
+          myself on my ability to create software that is both{" "}
+          <span className="bg-green-600">elegant</span> and{" "}
+          <span className="bg-green-600">functional</span>. When it comes to
+          software engineering, I'm not afraid of the dark. In fact, it's
+          against a <span className="bg-green-600">dark background</span> that
+          my technical skills and creative spirit shine brightly. I navigate the
+          complex world of software engineering with ease and precision, and I'm
+          always looking for new challenges to tackle. Whether I'm working on a
+          mobile app or a web-based project, my commitment to{" "}
+          <span className="bg-green-600">excellence</span> and attention to{" "}
+          <span className="bg-green-600">detail</span> are evident in every line
+          of code I write. I believe that the key to success in software
+          development is a combination of{" "}
+          <span className="bg-green-600">technical skill</span> and{" "}
+          <span className="bg-green-600">creative thinking</span>, and I'm
+          constantly pushing myself to develop both. In short, I'm a software
+          engineer who is passionate about what I do, and I'm committed to
+          delivering the{" "}
+          <span className="bg-green-600">best possible results</span> for my
+          clients. If you're looking for a skilled and dedicated developer to
+          bring your project to life, I'd love to hear from you.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
